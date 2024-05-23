8168d4d29d53aacf0e2f214f8302c197516bd7cc
Herlan <herlanherbas@gmail.com>
2024-05-23 16:17:27 -0400

    update: App

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Counter } from './components/Counter'
+import { ButtonComponent } from './components/ButtonComponent'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [ inputValue, setInputValue] = useState(1);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(Number(event.target.value))
+  }
 
   return (
     <>
@@ -18,10 +25,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-      <h2>{count}</h2>
-        <button onClick={() => setCount(count +1)}>Sumar</button>
-        <button onClick={() => setCount(count -1)}>Restar</button>
-        <button onClick={() => setCount(0)}>Reset</button>
+      <input type="number" value={inputValue} onChange={handleInputChange} />
+      <Counter count={count} />
+        <ButtonComponent onClick={() => setCount(count + inputValue)} label='Sumar'/>
+        <ButtonComponent onClick={() => setCount(count - inputValue)} label='Restar'/>
+        <ButtonComponent onClick={() => setCount(0)} label='Reset'/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
